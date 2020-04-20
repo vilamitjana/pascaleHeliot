00e681dd1eecef9c180dd00c0517030d8b33e11b
José Vilamitjana <vilamitjana.jose@gmail.com>
2020-04-20 16:51:33 +0200

    About mobile fixes

--- js/carousels.js
+++ js/carousels.js
@@ -1,11 +1,10 @@
-
 // ========================================================================= //
 //  Owl Carousel Services
 // ========================================================================= //
 
 
 $('.services-carousel').owlCarousel({
-    autoplay: false,
+    autoplay: true,
     loop: true,
     margin: 20,
     dots: false,
@@ -19,8 +18,7 @@ $('.services-carousel').owlCarousel({
 //  Image Gallary
 // ========================================================================= //
 const flechas = {
-    arrowLeft:
-        '<button data-fancybox-prev class="fancybox-button fancybox-button--arrow_left" title="{{PREV}}">' +
+    arrowLeft: '<button data-fancybox-prev class="fancybox-button fancybox-button--arrow_left" title="{{PREV}}">' +
         '<div>' +
         '<svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 64 80" x="0px" y="0px">' +
         '<polygon points="35.35 43.77 21.45 32 35.35 20.24 36.65 21.77 24.55 32 36.65 42.24 35.35 43.77"/>' +
@@ -28,8 +26,7 @@ const flechas = {
         '</svg>' +
         '</div>' +
         "</button>",
-    arrowRight:
-        '<button data-fancybox-next class="fancybox-button fancybox-button--arrow_right" title="{{NEXT}}">' +
+    arrowRight: '<button data-fancybox-next class="fancybox-button fancybox-button--arrow_right" title="{{NEXT}}">' +
         '<div>' +
         '<svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 64 80" x="0px" y="0px">' +
         '<polygon points="28.65 43.77 27.35 42.24 39.45 32 27.35 21.77 28.65 20.24 42.55 32 28.65 43.77"/>' +
@@ -41,14 +38,12 @@ const flechas = {
 };
 
 const flechas2 = {
-    arrowLeft:
-        '<button data-fancybox-prev class="fancybox-button fancybox-button--arrow_left" title="{{PREV}}">' +
+    arrowLeft: '<button data-fancybox-prev class="fancybox-button fancybox-button--arrow_left" title="{{PREV}}">' +
         '<div>' +
         '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns" viewBox="0 0 30 37.5" version="1.1" x="0px" y="0px"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage"><g sketch:type="MSArtboardGroup" transform="translate(-45.000000, -720.000000)" fill="#000000"><path d="M70,745.5 C70,745.776142 69.7761424,746 69.5,746 C69.4093371,746 69.3243102,745.97587 69.2509914,745.933681 L49.2590406,734.938108 C49.0170803,734.80503 48.9288139,734.501001 49.061892,734.259041 C49.1106657,734.170361 49.1824024,734.102327 49.2656303,734.058268 L69.2590406,723.061892 C69.501001,722.928814 69.8050298,723.01708 69.938108,723.259041 C69.9818001,723.338481 70.0016327,723.424612 70,723.509186 L70,745.5 Z M50.5375193,734.5 L69,744.654364 L69,724.345636 L50.5375193,734.5 Z" sketch:type="MSShapeGroup"/></g></g></svg>' +
         '</div>' +
         "</button>",
-    arrowRight:
-        '<button data-fancybox-next class="fancybox-button fancybox-button--arrow_right" title="{{NEXT}}">' +
+    arrowRight: '<button data-fancybox-next class="fancybox-button fancybox-button--arrow_right" title="{{NEXT}}">' +
         '<div>' +
         '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns" viewBox="0 0 30 37.5" version="1.1" x="0px" y="0px"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage"><g sketch:type="MSArtboardGroup" transform="translate(-90.000000, -720.000000)" fill="#000000"><path d="M115,745.5 C115,745.776142 114.776142,746 114.5,746 C114.409337,746 114.32431,745.97587 114.250991,745.933681 L94.2590406,734.938108 C94.0170803,734.80503 93.9288139,734.501001 94.061892,734.259041 C94.1106657,734.170361 94.1824024,734.102327 94.2656303,734.058268 L114.259041,723.061892 C114.501001,722.928814 114.80503,723.01708 114.938108,723.259041 C114.9818,723.338481 115.001633,723.424612 115,723.509186 L115,745.5 Z M95.5375193,734.5 L114,744.654364 L114,724.345636 L95.5375193,734.5 Z" sketch:type="MSShapeGroup" transform="translate(104.500000, 734.499953) scale(-1, 1) translate(-104.500000, -734.499953) "/></g></g></svg>' +
         '</div>' +
